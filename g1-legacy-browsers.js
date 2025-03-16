3b12752812c94cf2c0c3e75feb7aa9f3e0973774
PhnPsychoLingLab <2011aphasia@gmail.com>
2025-03-17 04:04:47 +0800

    Add files via upload

--- g1-legacy-browsers.js
+++ g1-legacy-browsers.js
@@ -126,7 +126,7 @@ psychoJS.start({
   ]
 });
 
-psychoJS.experimentLogger.setLevel(core.Logger.ServerLevel.INFO);
+psychoJS.experimentLogger.setLevel(core.Logger.ServerLevel.EXP);
 
 
 var currentLoop;
@@ -186,7 +186,7 @@ async function experimentInit() {
   hello_next_page = new visual.ButtonStim({
     win: psychoJS.window,
     name: 'hello_next_page',
-    text: None,
+    text: null,
     fillColor: [0.0, 0.0, 0.0],
     borderColor: [0.0, 0.0, 0.0],
     color: [0.0, 0.0, 0.0],
@@ -218,7 +218,7 @@ async function experimentInit() {
   exp1_intro_next_page = new visual.ButtonStim({
     win: psychoJS.window,
     name: 'exp1_intro_next_page',
-    text: None,
+    text: null,
     fillColor: [0.0, 0.0, 0.0],
     borderColor: [0.0, 0.0, 0.0],
     color: [0.0, 0.0, 0.0],
